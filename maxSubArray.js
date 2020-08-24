0214b03e9ea70ffd0f13105f814ed904bd8f97e3
ColeHuntley83 <calrugby1@gmail.com>
2020-08-24 16:20:46 -0700

    optimal solution sliding window

--- maxSubArray.js
+++ maxSubArray.js
@@ -4,19 +4,38 @@
 
 let data = [2, 3, 3, 5, 6, 1, 1];
 
-const maxSubArray = (arr, num) => {
-    if (arr.length < num || arr.length === 0) return null;
+// const maxSubArray = (arr, num) => {
+//     if (arr.length < num || arr.length === 0) return null;
 
-    let max = -Infinity;
-    for (let i = 0; i <= arr.length - num ; i++) {
-        let sum = 0;
-        for (let j = 0; j < num; j++) {
-            sum += arr[i + j];
-        };
-        max = Math.max(sum, max);
+//     let max = -Infinity;
+//     for (let i = 0; i <= arr.length - num ; i++) {
+//         let sum = 0;
+//         for (let j = 0; j < num; j++) {
+//             sum += arr[i + j];
+//         };
+//         max = Math.max(sum, max);
+//     }
+//     return max;
+// };
+
+const maxSubArray= (arr, num) => {
+    if (arr.length === 0 || arr.length< num) return null;
+    let maxSum = 0;
+    let tempSum = 0;
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i];
+        debugger;
+
+    };
+    for (let i = num; i < arr.length; i++) {
+        tempSum = maxSum + arr[i] - arr[i-num];
+        maxSum = Math.max(tempSum, maxSum);
     }
-    return max;
-};
+    return maxSum;
+    
 
-let result = maxSubArray(data, 4);
+}
+
+
+let result = maxSubArray(data, 2);
 console.log(result)
