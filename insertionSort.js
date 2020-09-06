390773e0c640f4f0c0806d9f1376306821ea188e
ColeHuntley83 <calrugby1@gmail.com>
2020-09-06 12:38:03 -0700

    import

---
+++ insertionSort.js
@@ -0,0 +1,16 @@
+import {returnLargeArr} from '/utils.js'
+
+const insertion = (arr) => {
+    for (let i = 1; i < arr.length; i++) {
+        let currentVal = arr[i];
+        for (let j = i -1; j >=0 && arr[j] > currentVal; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[j + 1] = currentVal;
+
+    }
+    return arr;
+};
+
+
+let result = insertion(returnLargeArr(10000));
---
+++ utils.js
@@ -0,0 +1,10 @@
+export default function returnLargeArr(num) {
+    let storage = [];
+    for (let i = 0; i < num; i++) {
+        let randonNum = Math.floor(Math.random() * 20000 - 200);
+        storage.push(randonNum);
+    };
+    return storage
+};
+
+
